0bacfc285ab4d96441abe686bffb55ad1a6b96c4
wangyawei <1796609990@qq.com>
2019-03-19 15:33:09 +0800

    ww

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-vue koa移动端商城
+##关于
+这是一个vue koa移动端商城的项目，主要是学了koa之后想结合vue做个项目实践一下，不太难，不算复杂
 
-## Build Setup
+##技术栈
+vue+webpack+koa+MongoDB+vantui
+
+##接口
+我用的nodemon 启动nodejs项目，还可以用 PM2
+cd service 
+nodemon index.js
+
+## 项目运行
 
 ``` bash
 # install dependencies
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ShoppingMall from '@/components/pages/ShoppingMall'
-import Main from '@/components/pages/Main'
-import Register from '@/components/pages/Register'
-import Login from '@/components/pages/Login'
-import Goods from '@/components/pages/Goods'
-import CategoryList from '@/components/pages/CategoryList'
-import Cart from '@/components/pages/Cart'
+// import ShoppingMall from '@/components/pages/ShoppingMall'
+// import Main from '@/components/pages/Main'
+// import Register from '@/components/pages/Register'
+// import Login from '@/components/pages/Login'
+// import Goods from '@/components/pages/Goods'
+// import CategoryList from '@/components/pages/CategoryList'
+// import Cart from '@/components/pages/Cart'
 import Member from '@/components/pages/Member'
 Vue.use(Router)
+
+const Main = r => require.ensure([], () => r(require('@/components/pages/Main')), 'Main')
+const ShoppingMall= r => require.ensure([],() => r(require('@/components/pages/ShoppingMall')),'ShoppingMall')
+const Register = r => require.ensure([], () => r(require('@/components/pages/Register')),'Register')
+const Login = r => require.ensure([], () => r(require('@/components/pages/Login')),'Login')
+const  Goods = r => require.ensure([], () => r(require('@/components/pages/Goods')),'Goods')
+const  CategoryList = r => require.ensure([], () => r(require('@/components/pages/CategoryList')),'CategoryList')
+const  Cart = r => require.ensure([], () => r(require('@/components/pages/Cart')),'Cart')
+const  Member = r => require.ensure([], () => r(require('@/components/pages/Member')),'Member')
 
 export default new Router({
   routes: [
